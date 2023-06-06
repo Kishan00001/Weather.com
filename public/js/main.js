cc561924261e3f793e723ea59aa74ef89ef85ca0
Kishan Kumar Maharana <kishanmaharana04@gmail.com>
2023-06-06 23:31:23 +0530

    secure now

--- public/js/main.js
+++ public/js/main.js
@@ -14,7 +14,7 @@ const getInfo = async(event) => {
         datahide.classList.add("data_hide");
     }else{
         try{ 
-            let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityVal}&appid=b0133a628594e511ddf9eaa349aa65c7&units=metric`;
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityVal}&appid=b0133a628594e511ddf9eaa349aa65c7&units=metric`;
             const response = await fetch(url);
 
             const data = await response.json();
